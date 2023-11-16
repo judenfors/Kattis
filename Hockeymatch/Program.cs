40e7d07016349e9c9b5495968d0ef318faee3af9
judenfors <rudenfors.eric@gmail.com>
2023-11-16 13:28:05 +0100

    Initial commit

---
+++ Hockeymatch/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[]args)
+    {
+        string input1 = Console.ReadLine();
+        string[] inputs = input1.Split(' ');
+
+        int saves = int.Parse(inputs[0]);
+        int goals = int.Parse(inputs[1]);
+
+    }
+}
---
+++ Äpple Päron/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[]args)
+    {
+        string input = Console.ReadLine();
+        string[] inputs = input.Split(' ');
+
+        int a = int.Parse(inputs[0]);
+        int p = int.Parse(inputs[1]);
+
+
+
+        if (a*7 < p*13) 
+        {
+            Console.WriteLine("petra");
+        }
+
+        else if (p*13 < a*7)
+        {
+            Console.WriteLine("axel");
+        }
+
+        else
+        {
+            Console.WriteLine("lika");
+        }
+    }
+}
